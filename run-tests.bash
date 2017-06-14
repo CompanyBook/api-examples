efe36eaa5c92c770823d83128a85d5563f490172
Oleg Blinnikov <osblinnikov@gmail.com>
2017-06-14 15:13:49 +0200

    fix for tests

--- run-tests.bash
+++ run-tests.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "API_KEY='$1'" > python/search/authority.py
+echo "API_KEY='$1'" > python/similar_companies/authority.py
 
 python python/search/service.py industry database GB
 python python/search/service.py search database GB 511210
