2e48f65aef7b8af54da634908d5f8ebc34f5098b
Oleg Blinnikov <osblinnikov@gmail.com>
2017-06-14 15:44:44 +0200

    use python3.5

--- run-tests.bash
+++ run-tests.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-python --version
-pip install requests
+python3.5 --version
+pip3.5 install requests
 
 echo "API_KEY='$1'" > python/search/authority.py
 echo "API_KEY='$1'" > python/similar_companies/authority.py
 
-python python/search/service.py industry database GB
-python python/search/service.py search database GB 511210
-python python/search/service.py match apple
-python python/similar_companies/service.py US0000000060704780
+python3.5 python/search/service.py industry database GB
+python3.5 python/search/service.py search database GB 511210
+python3.5 python/search/service.py match apple
+python3.5 python/similar_companies/service.py US0000000060704780
