77299254f4732c2020200c9fa91f565095fcfe4e
Oleg Blinnikov <osblinnikov@gmail.com>
2017-06-14 15:48:20 +0200

    remove x-api-key creation

--- run-tests.bash
+++ run-tests.bash
@@ -3,9 +3,6 @@
 python3.5 --version
 pip3.5 install requests
 
-echo "API_KEY='$1'" > python/search/authority.py
-echo "API_KEY='$1'" > python/similar_companies/authority.py
-
 python3.5 python/search/service.py industry database GB
 python3.5 python/search/service.py search database GB 511210
 python3.5 python/search/service.py match apple
