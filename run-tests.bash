9665bcdcc44fe44db9bdc32ad4b7e4038df9d02c
Knut O. Hellan <knut@companybook.no>
2017-06-26 13:19:05 +0200

    Improve testing
    Add test of the news relation service.

--- run-tests.bash
+++ run-tests.bash
@@ -12,3 +12,4 @@ python3.5 python/search.py industry database GB
 python3.5 python/search.py search database GB 511210
 python3.5 python/search.py match apple
 python3.5 python/similar_companies.py US0000000060704780
+python3.5 python/news.py relation US0000000060704780 competitors
