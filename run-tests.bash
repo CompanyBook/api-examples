1dddc820fbbd8026883b10e38d7838d3af6715ef
Knut O. Hellan <knut@companybook.no>
2017-09-18 15:57:39 +0200

    Bugfix reset comments
    Reenabled commented code in run-tests.bash

--- run-tests.bash
+++ run-tests.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#export LC_ALL=en_US.UTF-8
-#export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 
-#set -e
+set -e
 
-#python3.5 --version
-#pip3.5 install requests
+python3.5 --version
+pip3.5 install requests
 
 python3.5 python/search.py industry database GB
 python3.5 python/search.py search database GB 511210
